61947944c6281af643891453411f6dd067e045b2
iAbdulhamid <i_abdulhamid@outlook.com>
2019-07-28 13:16:19 +0200

    setting up the app - adding the layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout-components/header/header.component';
+import { FooterComponent } from './layout-components/footer/footer.component';
+import { ContentComponent } from './layout-components/content/content.component';
+import { SidebarComponent } from './layout-components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
